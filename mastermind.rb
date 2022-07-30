087719250b60ba081f51f5eac1296822fc85534d
Enntek <abluecar@gmail.com>
2022-07-29 22:19:06 -0400

    Reveal code on win

--- mastermind.rb
+++ mastermind.rb
@@ -3,26 +3,23 @@
 # Goal: Build a console based version of the game Mastermind
 # My settings: 6 numbers, 10 guesses
 
-# readability, modularity, brevity
 # the <super> in the baseclass Class Method refers to the Superclass's Class Method too.
-
 
 # add:
 # reveal code when player wins
 # work on computer play
-# remove "Current turn" after win
-# move "you win" message below game_board
+# remove 'Current turn' after win
+# move 'you win' message below game_board
 # make some private/public methods to get used to making these
+# try namespacing using modules
+# include 1 module's method
 
+# overwrite string with a substring, does not change length
 class String
   def insert_overwrite(str, index)
-    # given str and index
-    # "overwrite" existing string at index
-    # new string should be same length as old string. 
-    # use .length to overwrite the same num of chars
-    # we may reuse this one
+    self.insert(index, str)
+    self[0..(index + str.length - 1)] + self[(index + str.length + str.length)..-1]
   end
-
 end
 
 
@@ -37,16 +34,17 @@ class BoardGame
 end
 
 class Mastermind < BoardGame
-  @@name = "Mastermind"
+  @@name = 'Mastermind'
   attr_reader :current_turn
 
   def initialize
     @secret_code = []
     @turn_number = 1
+    @game_over = false
     human = Human.new
     computer = Computer.new
     establish_secret_code(computer.code)
-    human.speak
+    # human.speak
     @current_turn = "\t Current turn | * * * * |" 
     @all_turns = Array.new(10, "\t              | * * * * |")
     @all_turns[0] = @current_turn
@@ -64,11 +62,16 @@ class Mastermind < BoardGame
 
   def establish_secret_code(code)
     @secret_code = code
-    puts "Secret code established!!"
+    puts 'Secret code established!!'
   end
 
   def draw_board
-    puts "\n\t                X X X X "
+    
+    if @game_over == true
+      puts "\n\t                #{@secret_code[0]} #{@secret_code[1]} #{@secret_code[2]} #{@secret_code[3]} "
+    else
+      puts "\n\t                X X X X "
+    end
     
     for i in (0..@all_turns.length-1).reverse_each
       puts "\t              |---------|"
@@ -82,7 +85,10 @@ class Mastermind < BoardGame
     num_of_red_pegs = 0
     num_of_white_pegs = 0
 
-    if player.guess == @secret_code then puts "\n<< You cracked the code! You won! >>\n" end
+    if player.guess == @secret_code
+      puts "\n<< You cracked the code! You won! >>\n"
+      @game_over = true
+    end
     
     num_of_red_pegs = @secret_code.each_index.reduce(0) do |total_red, index|
       if player.guess[index] == @secret_code[index]
@@ -96,9 +102,9 @@ class Mastermind < BoardGame
     num_correct_digits = 4 - wrong_code_digits.length
     num_of_white_pegs = num_correct_digits - num_of_red_pegs
     
-    pegs = ""
-    num_of_red_pegs.times { pegs += "R " }
-    num_of_white_pegs.times { pegs += "W " }
+    pegs = ''
+    num_of_red_pegs.times { pegs += 'R ' }
+    num_of_white_pegs.times { pegs += 'W ' }
 
     if @turn_number <= 10  then
       @turn_number += 1
@@ -106,40 +112,40 @@ class Mastermind < BoardGame
 
     @all_turns[@turn_number-2] = "\t              | #{player.guess[0]} #{player.guess[1]} #{player.guess[2]} #{player.guess[3]} | #{pegs}" 
     
-    # insert "Current turn"
+    # insert 'Current turn'
     if @turn_number <= 10 then
       @all_turns[@turn_number-1] = @current_turn
     end
 
-    # insert "Game over" after last turn
+    # insert 'Game over' after last turn
     if @turn_number == 11 then
       # change string on last turn
       s1 = @all_turns[@turn_number-2][0..2]
       s2 = @all_turns[@turn_number-2][12..-1]
-      @all_turns[@turn_number-2] = s1 + "Game Over" + s2
+      @all_turns[@turn_number-2] = s1 + 'Game Over' + s2
     end
 
   end
   
   def self.description
     super
-    puts "<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>"
+    puts '<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>'
     puts "<< Mastermind is a classic 2 player boardgame. >> \n\n<< One player is the code-setter, and the other the codebreaker. >>\n "
     puts "<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>\n "
   end
   
   def self.history
-    "This game dates back a century when it was then called Bulls & Cows."
+    'This game dates back a century when it was then called Bulls & Cows.'
   end
 end
 
 class Player
   def description
-    puts "A game will have x number of players"
+    puts 'A game will have x number of players'
   end
 
   def take_turn
-    puts "Enter 4 digits (1 to 6) and check your guess:"
+    puts 'Enter 4 digits (1 to 6) and check your guess:'
   end
 end
 
@@ -150,13 +156,13 @@ class Human < Player
   end
 
   def speak
-    puts "I am made of flesh and blood!"
+    puts 'I am made of flesh and blood!'
   end
 
   def take_turn
     super
     @guess = gets.chomp
-    @guess = @guess.split("")
+    @guess = @guess.split('')
   end
 end
 
@@ -168,7 +174,7 @@ class Computer < Player
   end
 
   def set_code
-    digit_bank = ["1", "2", "3", "4", "5", "6"]
+    digit_bank = ['1', '2', '3', '4', '5', '6']
     code_arr = []
 
     4.times do 
@@ -178,11 +184,11 @@ class Computer < Player
     end
 
     @code = code_arr
-    # @code = "1234"
+    # @code = ['1', '2', '3', '4']
   end
 
   def speak
-    puts "I am made of 1s and 0s."
+    puts 'I am made of 1s and 0s.'
   end
 end
 
--- temp.rb
+++ temp.rb
@@ -1,59 +1,14 @@
-class BoardGame
-  def initialize(note_to_self)
-    @note_to_self = note_to_self
-    puts "BoardGame has been instantiated."
+require 'pry-byebug'
 
-    puts @note_to_self
-  end
-
-  def display_message
-    puts "I'm a boardgame!"
+class String
+  def insert_overwrite(str, index)
+    self.insert(index, str)
+    self[0..(index + str.length - 1)] + self[(index + str.length + str.length)..-1]
   end
 end
 
-class Mastermind < BoardGame
-
-  def initialize(version, note)
-    super(note)
-    @version = version
-  end
-
-  def display_message
-    super
-    puts "Mastermind is great fun!"
-  end
-end
-
-newgame = Mastermind.new("1.4", "buy another game")
-newgame.display_message
 
 
-# 10.step(0, -1) do |i|
-#   puts i 
-# end
+p "hey, hello world!".insert_overwrite("heywhatisthisthing", 5)
 
-# for i in (0..10).reverse_each
-#   puts i 
-# end
-
-str = "|0123456789|"
-# add text to str
-
-s1= str[0..5]
-s2= str[6..-1]
-
-# new_str = str[5..-1]
-
-# new_str = s1 + "hello" + s2
-
-# puts new_str
-
-digit_bank = [1, 2, 3, 4, 5, 6]
-code_arr = []
-4.times do 
-  rand_digit = digit_bank.sample
-  code_arr.push(rand_digit)
-  digit_bank.delete(rand_digit)
-end
-p code_arr
-p digit_bank
+#=> 'hey, wazza world!'
